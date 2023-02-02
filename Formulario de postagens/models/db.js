4e724fc882002820251cd71d8f6471bd0014ad30
Hector Jimenez <euhectorjimenez@gmail.com>
2023-02-02 09:35:06 -0400

    Formulário de postagens.
    Formulário de postagens feito em Node, MySQL com os frameworks express e sequelize.

---
+++ Formulario de postagens/models/db.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+
+// Conexão com o banco de dados MySQL
+const sequelize = new Sequelize('postapp', 'root', '1620', {
+    host: 'localhost',
+    dialect: 'mysql',
+    query: { raw: true }
+})
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}
